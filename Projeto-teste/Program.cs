cdbf419337032ce3d7ed90764c04675fab509b46
Marco Di Veroli <madive60@gmail.com>
2021-01-06 10:38:25 -0300

    Acrescentada a pergunta sobre como voce esta hoje

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -5,6 +5,7 @@ namespace Projeto_teste {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje? ");
         }
     }
 }
