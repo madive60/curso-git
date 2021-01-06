3be8bfd555b7a79348edb554b5f70daa5e55d8db
Marco Di Veroli <madive60@gmail.com>
2021-01-06 12:02:42 -0300

    Modificada a estrutura do codigo

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace Projeto_teste {
-    class Program {
-        static void Main(string[] args) {
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje? ");
