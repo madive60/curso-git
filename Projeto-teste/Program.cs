a09e681ed52465c44144af8e20ada4bf69bb13cf
Marco Di Veroli <madive60@gmail.com>
2021-01-06 11:32:03 -0300

    Acrescentada a mensagem desejando um bom dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -6,6 +6,7 @@ namespace Projeto_teste {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje? ");
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
